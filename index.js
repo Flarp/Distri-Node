c7141e31d8c8629479ea428c62378152fcab6bc6
Flarp <flurpdadurp@gmail.com>
2016-10-06 21:25:18 +0000

    clean up code and comment it a little

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ class DistriServer extends EventEmitter {
         super()
         if (opts.constructor.name !== 'Object') throw new TypeError('Options must be given in the form of an object')
         
+        
+        // explained in the README
         this.options = defaults(opts, {
             port: 8080,
             
@@ -26,20 +28,35 @@ class DistriServer extends EventEmitter {
         
         this.server = new ws.Server({port:this.options.port})
         
+        // a number of the solved problems
         this.solutions = 0;
+        
+        // a number of problems with the maximum of clients and
+        // a number of solved problems
         this.fullProblems = 0;
+        
+        // how many connected users there are
         this.userCount = 0;
+        
+        // A cue of websocket client objects if the
+        // user count is not met.
         this.userQueue = [];
         
+        // where the work is stored, along with all its metadata
         this.session = this.options.work.map(work => {
             return {workers:0, work:work, solutions:[]}
         })
         
-        let usingEmpty = false
-        
+        // To speed things up, when only 5% of work is left, an array of the indexes of available work
+        // is created. Trying to get a random index in an array with only one available
+        // will take a very long time. 
         const empty = new Float32Array(Math.ceil(this.session.length/20))
         
+        // is the above array being used in place of randomly generating indexes?
+        let usingEmpty = false
+        
         setInterval(() => {
+            // check every two minutes of there is less than 5% of work left
             if (!usingEmpty && (this.session.length*(19/20))<this.solutions) {
                 let lastInd = -1;
                 this.session.map((val, ind) => val.solution.length !== this.options.security.verificationStrength ? empty[lastInd++] = ind : null )
@@ -51,28 +68,40 @@ class DistriServer extends EventEmitter {
             let index;
             
             if (usingEmpty) {
+                // if we are using the empty array
                 let temp = Math.floor(Math.random() * empty.length)
                 index = empty[temp]
             } else {
+                // get a random index from the session array.
                 index = Math.floor(Math.random() * this.session.length)
             }
-            if(this.solutions === this.options.work.length || this.fullProblems === this.options.work.length) return -1
+            // if everything is full
+            if(this.fullProblems >= this.options.work.length) return -1
             return ((this.session[index].workers + this.session[index].solutions.length) >= this.options.security.verificationStrength)
-            ? randomIndGenerator()
-            : index
+            ? randomIndGenerator() // get another random index if that problem is full
+            : index // return that index
         }
         
         this.server.on('connection', (ws) => {
+            // Generate a starting index in the session array. Starting at -1
+            // will let the server know if someone unverified is trying to 
+            // request work
             let ind = -1;
+            let stage = 0;
+            // generate something random for later on
             let gen = idgen()
             this.userCount++
             
+            // if the minimum user count has not been met
             if (this.userCount < this.options.security.minUsers) {
+                // queue the user 
                 this.userQueue.push(ws)
             } else {
+                // tell each user in the queue that they can now request
                 this.userQueue.map(user => user.send(msg.pack({responseType:'request'}), {binary:true}))
                 this.userQueue = [];
                 ws.send(msg.pack({responseType:'request'}), {binary:true})
+                stage = 1;
             }
             
             ws.on('message', (m) => {
@@ -83,13 +112,18 @@ class DistriServer extends EventEmitter {
                 }
                 
                 const message = msg.unpack(m);
+                // if anything is wrong with the message
                 if ((message.constructor !== Object) || !message.response || !message.responseType) {
-                    console.log(message.constructor)
+                    // kick the user if the server is using strict mode
                     if(this.options.security.strict) ws.close()
                     return;
                 }
                 switch(message.responseType) {
                     case "request":
+                        if (stage !== 1) { // if the user is not at the correct stage
+                            if(this.options.security.strict) ws.close() // kick the user if strict mode is on
+                            return;
+                        }
                         ws.send(msg.pack({data:[gen,this.options.security.hashStrength],responseType:'submit_hash'}))
                         break;
                     case "submit_hash":
@@ -103,10 +137,12 @@ class DistriServer extends EventEmitter {
                                 ws.send(msg.pack({responseType:'submit_work', work:this.session[ind].work}))
                             }
                             
-                        } else if (this.options.security.strict) ws.close()
-                        else {
-                            console.log('fail')
-                        }
+                        } else if (this.options.security.strict) {
+                            // if the hash is wrong and strict mode is on.
+                            ws.close();
+                            return;
+                        } else return;
+                        
                         break;
                         
                     case 'submit_work':
